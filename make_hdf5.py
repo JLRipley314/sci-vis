a6daed3b6731467392f60801a90243f14733260d
Justin Ripley <lloydripley@gmail.com>
2020-03-03 17:30:46 -0500

    some name changes and change of buttons

--- make_hdf5.py
+++ make_hdf5.py
@@ -3,7 +3,7 @@ import numpy as np
 from io_hdf5 import *
 
 #-----------------------------------------------------------------------------
-### example implementation
+## example implementation
 #-----------------------------------------------------------------------------
 if __name__ == '__main__':
 	init_hdf5('example_1D.h5')
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 		vals=   np.random.random(size=(100))
 		
 		write_line_hdf5('example_1D.h5',time,vals)
-
+#-----------------------------------------------------------------------------
 	init_hdf5('example_2D_01.h5')
 	for i in range(100):
 		time= i
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 			for y in y_coords
 		])
 		write_line_hdf5('example_2D_01.h5',time,vals)
-
+#-----------------------------------------------------------------------------
 	init_hdf5('example_2D_02.h5')
 	for i in range(100):
 		time= i
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 		y_coords = np.linspace(-2*np.pi, 2*np.pi, rows+1)
 
 		vals= np.array([
-			[np.cos(0.01*time*x*y) for x in x_coords]
+			[np.sin(0.01*time*x*y)*10*np.cos(2*np.pi*i/50) for x in x_coords]
 			for y in y_coords
 		])
 		write_line_hdf5('example_2D_02.h5',time,vals)
--- plotter_1d_hdf5.py
+++ plotter_1d_hdf5.py
@@ -1,21 +1,16 @@
-#-----------------------------------------------------------------------------
-import sys
-import numpy as np
-#-----------------------------------------------------------------------------
 from PyQt5 import QtGui, QtCore
 import pyqtgraph as pg
-#-----------------------------------------------------------------------------
+import pyqtgraph.console
+import numpy as np
+import pandas as pd
+import sys
+
 ### make a widget for displaying 3D objects
 import pyqtgraph.opengl as gl
-import pyqtgraph.console
-#-----------------------------------------------------------------------------
-## for manipulating hdf5 
+
+### for manipulating hdf5 
 from io_hdf5 import *
-#-----------------------------------------------------------------------------
-## for coloring of solutions 
-import matplotlib.pyplot as plt
-cmap= plt.get_cmap('jet')
-rgba_img = cmap(1.)
+
 ##############################################################################
 ##############################################################################
 class Plotter(QtGui.QWidget):
@@ -32,20 +27,20 @@ class Plotter(QtGui.QWidget):
 		self.initUI()
 ##############################################################################
 	def initUI(self):
-#----------------------------------------------------------------------------
+##############################################################################
 		self.setWindowTitle('Plotter')
 		self.keyPressed.connect(self.on_key)
-#----------------------------------------------------------------------------
-## minimum size when opening gui
+##############################################################################
+# minimum size when opening gui
 		self.setGeometry(self.left, self.top, self.width, self.height)
-#----------------------------------------------------------------------------
-## Create a grid layout to manage the widgets size and position
-## allows for plot window to dynamically resize with size of gui
+##############################################################################
+# Create a grid layout to manage the widgets size and position
+# allows for plot window to dynamically resize with size of gui
 		self.layout = QtGui.QGridLayout()
 		self.setLayout(self.layout)
-#----------------------------------------------------------------------------
+##############################################################################
 # Create some widgets to be placed inside
-#----------------------------------------------------------------------------
+##############################################################################
 #TODO: testing this -- console widget
 		text = """
 		Python 3 console. Namespace includes numpy as 'np', pyqtgraph as 'pg', and 'self'. Data loaded through the 'Select Files' button is stored in 'self.var_arr'. Plot directly to built-in window with self.plotWindow.plot() and plot to pop-out window with pg.plot().
@@ -55,52 +50,52 @@ class Plotter(QtGui.QWidget):
 			pyqtgraph.console.ConsoleWidget(
 				namespace=NS, text=text),6, 0, 3, 1
 		)
-#----------------------------------------------------------------------------
+##############################################################################
 		self.load_btn = QtGui.QPushButton('Select Files', self)
 		self.load_btn.clicked.connect(self.handleButton)
 		self.layout.addWidget(self.load_btn, 0, 0)
-#----------------------------------------------------------------------------
+##############################################################################
+		self.step_label = QtGui.QLabel('time step size: ', self)
+		self.layout.addWidget(self.step_label, 1, 0)
+##############################################################################
 		self.enter_step = QtGui.QLineEdit('1', self)
 		self.enter_step.setFixedWidth(100)
-		self.layout.addWidget(self.enter_step, 1, 0)
-#----------------------------------------------------------------------------
-		self.display_step = QtGui.QLabel('step = 00000', self)
-		self.layout.addWidget(self.display_step, 2, 0)
-#----------------------------------------------------------------------------
-		self.display_norm = QtGui.QLabel('norm = 00000', self)
-		self.layout.addWidget(self.display_norm, 3, 0)
-#----------------------------------------------------------------------------
+		self.layout.addWidget(self.enter_step, 2, 0)
+##############################################################################
+		self.display_n = QtGui.QLabel('time step = 00000', self)
+		self.layout.addWidget(self.display_n, 3, 0)
+##############################################################################
 		self.play_btn = QtGui.QPushButton('Play', self)
 		self.play_btn.setCheckable(True)
 		self.play_btn.clicked.connect(self.play_btn_state)
 		self.layout.addWidget(self.play_btn, 4, 0)
-#----------------------------------------------------------------------------
-## for 3d viewing		
-		self.plotWindow = gl.GLViewWidget(self)
-		self.plotWindow.setCameraPosition(distance=15)
-
+##############################################################################
+		self.plotWindow = pg.PlotWidget(self)
+		self.plotWindow.addLegend(offset=(25,25))
 		self.layout.addWidget(self.plotWindow, 0, 1, 9, 1)
-
-		self.plots= []
-#----------------------------------------------------------------------------
-## for playing the movies
+##############################################################################
+### for playing the movies
 		self.timer = QtCore.QTimer(self)
 		self.timer.timeout.connect(self.advance_n)
 		self.update_time_ms = 100
-#----------------------------------------------------------------------------
-## define global variables to be used by applet
+##############################################################################
+# define global variables to be used by applet
 		self.filename = ''
+		self.n = 0
 		self.step = 1
-		self.norm = 1
 		self.maxn = 0
 		self.plot_num = 0
+
+		self.step1 = 1
+		self.step2 = 1
+		self.step3 = 1
 
 		self.color = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 		self.penwidth = 3
 		# options for pointSymbol: None, 'x', 'o', 't', 't1', 't2'
 		#'t3', 's', 'p', 'h', 'star', '+', 'd'
 		self.pointSymbol = None
-#----------------------------------------------------------------------------
+##############################################################################
 		self.show()
 ##############################################################################
 	def play_btn_state(self):
@@ -110,9 +105,9 @@ class Plotter(QtGui.QWidget):
 			self.timer.stop()
 ##############################################################################
 	def advance_n(self):
-		if(self.step+1 < self.maxn):
-			self.step+= 1
-			self.update_plot_step()
+		if(self.n+self.step < self.maxn):
+			self.n += self.step
+			self.update_plot_n()
 		else:
 			self.timer.stop()
 			self.play_btn.toggle()
@@ -122,24 +117,28 @@ class Plotter(QtGui.QWidget):
 		paths, _ = QtGui.QFileDialog.getOpenFileNames(self, title)
 		for path in paths:
 			self.load_data(path)
-		self.update_plot_step()
 ##############################################################################
-##TODO:  operations on arrays.  It would be nice to implement this through
-##       the control window; have each loaded array be assigned to its own
-##       variable, and we could then plot |arr|, or arr.transpose(), etc.
+#TODO:  operations on arrays.  It would be nice to implement this through
+#       the control window; have each loaded array be assigned to its own
+#       variable, and we could then plot |arr|, or arr.transpose(), etc.
 ##############################################################################
 	def load_data(self, filename):
-		y = read_hdf5(str(filename))
+		x, y = read_hdf5(str(filename))
 		if (self.plot_num==0):
-			self.var_arr   = np.array([y])
+			self.var_arr   = y
 			self.var_names = self.prune_string(filename)
-			self.plot_data(self.var_arr[0])
+			self.plot_data(self.var_arr)
 		else:
-			self.var_arr   = np.append(self.var_arr, y)
+			self.var_arr   = np.append(self.var_arr,   y)
 			self.var_names = np.append(self.var_names, self.prune_string(filename))
 #-----------------------------------------------------------------------------
-## have to make array with correct indexing
-			if (len(y.shape)==3):
+### have to make array with correct indexing
+			if (len(y.shape)==2):
+				self.var_arr= np.reshape(
+					self.var_arr,
+					(self.plot_num+1, np.shape(y)[0], np.shape(y)[1])
+				)
+			elif (len(y.shape)==3):
 				self.var_arr= np.reshape(
 					self.var_arr,
 					(self.plot_num+1, np.shape(y)[0], np.shape(y)[1], np.shape(y)[2])
@@ -157,33 +156,26 @@ class Plotter(QtGui.QWidget):
 		name_str = str(filename)[loc_start:loc_end]
 		return name_str
 ##############################################################################
-	def plot_data(self, vals) -> None:
-		self.maxn = np.shape(vals)[0]
-		if (len(vals.shape)==3):
-			self.plots.append(
-				gl.GLSurfacePlotItem(
-					x= np.linspace(-5.0,5.0,vals[0].shape[0]),
-					y= np.linspace(-5.0,5.0,vals[0].shape[1]),
-					shader='heightColor',
-					computeNormals=False,
-					smooth=False
-				)
+	def plot_data(self, y) -> None:
+		self.maxn = np.shape(y)[0]
+		if (len(y.shape)==2):
+			self.plotWindow.plot(
+				y[self.n, :], 
+				pen=pg.mkPen(self.color[self.plot_num], width=self.penwidth),
+				symbol=self.pointSymbol, 
+				symbolBrush=self.color[self.plot_num], 
+				name=self.var_names[self.plot_num]
 			)
-			self.plots[-1].shader()['colorMap'] = np.array([
-				0.01, ## red 1
-				0,    ## red 2
-				0.01, ## red 3
-				0.5,  ## green 1
-				0,    ## green 2
-				0.5,  ## green 3
-				0.99, ## blue 1
-				0,    ## blue 2
-				0.99  ## blue 3
-			])
-			self.plots[-1].setData(z=vals[0])
-			self.plotWindow.addItem(self.plots[-1])
+		elif (len(y.shape)==3):
+			self.plotWindow.plot(
+				y[self.n, :, :], 
+				pen=pg.mkPen(self.color[self.plot_num], width=self.penwidth),
+				symbol=self.pointSymbol, 
+				symbolBrush=self.color[self.plot_num], 
+				name=self.var_names[self.plot_num]
+			)
 		else:
-			raise SystemError("len(y.shape)!=3")
+			raise SystemError("len(y.shape)!=2,3")
 		return
 ##############################################################################
 	def keyPressEvent(self, event):
@@ -191,48 +183,53 @@ class Plotter(QtGui.QWidget):
 		self.keyPressed.emit(event)
 		self.step = int(str(self.enter_step.text()))
 ##############################################################################
-	def update_plot_step(self):
-		self.compute_norm()
-		self.update_display()
+	def update_plot_n(self):
+		self.plotWindow.clear()
+		self.update_display_n()
+		plot_index = 0
 		if (self.plot_num<=0):
-			raise ValueError("self.plot_num<=0")
-		for i in range(self.plot_num):
-			self.plots[i].setData(
-				z=self.var_arr[i,self.step, :, :]/self.norm
+			raise SystemError("self.plot_num<=0")
+		elif (self.plot_num==1):
+			self.plotWindow.plot(
+#				self.coord_arr[self.n, :],
+				self.var_arr[self.n, :], 
+				pen=pg.mkPen(self.color[plot_index],width=self.penwidth),
+				symbol=self.pointSymbol, 
+				symbolBrush=self.color[plot_index]
 			)
+		else:
+			for i in range(self.var_arr.shape[0]):
+				self.plotWindow.plot(
+#					self.coord_arr[i,self.n, :],	
+					self.var_arr[  i,self.n, :],
+					pen=pg.mkPen(self.color[plot_index],width=self.penwidth),
+					symbol=self.pointSymbol, 
+					symbolBrush=self.color[plot_index]
+				)
+				plot_index += 1
 		return 0
 ##############################################################################
-	def compute_norm(self):
-		self.norm=0
-		for i in range(self.plot_num):
-			self.norm= max(
-				abs(self.var_arr[i,self.step, :, :]).max(),
-				self.norm
-			)
-		if (self.norm==0):
-			self.norm= 1
+	def update_display_n(self):
+		self.display_n.setText('n = ' + str(self.n))
 ##############################################################################
-	def update_display(self):
-		self.display_step.setText('t = ' + str(self.step))
-		self.display_norm.setText('norm = ' + str(self.norm))
-##############################################################################
-## if press 'enter' then +1 step
-## if press 'q' then closes the application
-## if press right/left arrow thentime step +1/-1
+# if press 'enter' then +1 step
+# if press 'q' then closes the application
+# if press right/left arrow thentime step +1/-1
 	def on_key(self, event):
 		if(event.key() == QtCore.Qt.Key_Enter):
 			self.step = int(str(self.enter_step.text()))
+			#print('step changed to'+str(self.step))
 		elif(event.key() == QtCore.Qt.Key_Q):
 			print('Exiting')
 			self.deleteLater()
 		elif(event.key() == QtCore.Qt.Key_Right):
-			if(self.step+1 < self.maxn):
-				self.step += 1 
-				self.update_plot_step()
+			if(self.n+self.step < self.maxn):
+				self.n += self.step
+				self.update_plot_n()
 			elif event.key() == QtCore.Qt.Key_Left:
-				if(self.step-1 >= 0):
-					self.step-= 1 
-					self.update_plot_step()
+				if(self.n-self.step >= 0):
+					self.n -= self.step
+					self.update_plot_n()
 ##############################################################################
 ##############################################################################
 def main():
@@ -243,4 +240,3 @@ def main():
 ##############################################################################
 if __name__ == '__main__':
 	main()
-
