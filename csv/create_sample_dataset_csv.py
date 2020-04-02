980380d4b1bfc9a45c73fe14c1dc580037bfb486
Justin Ripley <lloydripley@gmail.com>
2020-04-02 10:18:21 -0400

    fixed csv processing problem

--- csv/create_sample_dataset_csv.py
+++ csv/create_sample_dataset_csv.py
@@ -6,19 +6,19 @@ from io_csv import *
 ## example implementation
 #-----------------------------------------------------------------------------
 if __name__ == '__main__':
-        init_csv('./example_data/example_1d_1')
-        init_csv('./example_data/example_1d_2')
-        init_csv('./example_data/example_1d_3')
-        init_csv('./example_data/example_1d_4')
+        init_csv('example_1d_1')
+        init_csv('example_1d_2')
+        init_csv('example_1d_3')
+        init_csv('example_1d_4')
         for i in range(100):
                 vals= np.random.random(size=(100))    
-                write_line_csv_1d('./example_data/example_1d_1',vals)
+                write_line_csv_1d('example_1d_1',vals)
         for i in range(100):
                 vals= np.random.random(size=(100))    
-                write_line_csv_1d('./example_data/example_1d_2',vals)
+                write_line_csv_1d('example_1d_2',vals)
         for i in range(100):
                 vals= np.random.random(size=(100))    
-                write_line_csv_1d('./example_data/example_1d_3',vals)
+                write_line_csv_1d('example_1d_3',vals)
         for i in range(100):
                 vals= np.random.random(size=(100))    
-                write_line_csv_1d('./example_data/example_1d_4',vals)
+                write_line_csv_1d('example_1d_4',vals)
